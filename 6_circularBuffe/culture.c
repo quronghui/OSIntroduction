4a268cda9ce1963aa1d50e1b0d29c2aa6f437596
quronghui <quronghui@qq.com>
2019-08-09 19:15:17 +0800

    makegile

---
+++ 6_circularBuffe/culture.c
@@ -0,0 +1,13 @@
+/*
+*   1. 解题思路: 辗转相除法
+        a. 难点在于: 保存最大公约数的值, 通过字符串保存;  itoa
+        b. 字符串保存后, 如何计算除法呢?
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char string[] = 
+}
