415e44ae61a5ecc0f3e9bd69cbb5aaa2a7e1f8b5
quronghui <quronghui@qq.com>
2019-08-14 17:36:15 +0800

    mutex

--- 2_mutex_cond/mutex_api.c
+++ 2_mutex_cond/mutex_api.c
@@ -21,15 +21,26 @@ void Pthread_mutex_unlock( pthread_mutex_t *mutex ){
 }
 
 // // 临界区的使用
-// void *mythread(void *arg)
-// {
-// 	pthread_mutex_t lock;
-//   	Pthread_mutex_init(&lock);
-// 	// 领街区的代码使用
-// 	Pthread_mutex_lock(&lock);
-// 	int x = x+1;	
-// 	Pthread_mutex_unlock(&lock);
-// }
+void *mythread(void *arg)
+{
+	pthread_mutex_t lock;
+  	Pthread_mutex_init(&lock);
+	// 领街区的代码使用
+	Pthread_mutex_lock(&lock);
+	int x = x+1;	
+	Pthread_mutex_unlock(&lock);
+}
 
 
+// 创建两个子线程, 还必须得有主线程才行;
+int main(int argc, char *argv[])
+{
+    int rc;
+    printf("parent begin:   \n ");
 
+    pthread_t  p;
+    rc = pthread_create( &p, NULL, mythread, NULL ); assert(rc== 0);
+
+    rc = pthread_join(p, NULL); assert(rc== 0);       // 只需要等待一个子线程完成, 就能代表所有的吗?
+    printf("\nparent: end\n");
+}
